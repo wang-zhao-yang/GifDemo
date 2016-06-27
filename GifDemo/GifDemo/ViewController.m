a3a3a970d392300babba53b6be70c7641052684f
userWCY <wcy1107457658@163.com>
2016-06-27 09:52:10 +0800

    添加 SDWebImage 方法

--- GifDemo/GifDemo/ViewController.m
+++ GifDemo/GifDemo/ViewController.m
@@ -6,8 +6,11 @@
 //  Copyright © 2016年 chuanglong. All rights reserved.
 //
 
+#define kX [UIScreen mainScreen].bounds.size.width/2.0-100
+
 #import "ViewController.h"
 #import "GifView.h"
+#import <UIImage+GIF.h>
 
 @interface ViewController ()
 
@@ -18,20 +21,27 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // GifView
-    GifView *dataView = [[GifView alloc] initWithFrame:CGRectMake([UIScreen mainScreen].bounds.size.width/2.0-100, 30, 200, 150) data:[self loadDataForIndex:1]];
+    GifView *dataView = [[GifView alloc] initWithFrame:CGRectMake(kX, 20, 200, 150) data:[self loadDataForIndex:1]];
     [self.view addSubview:dataView];
+    
     // WebView
-    UIWebView *webView = [[UIWebView alloc] initWithFrame:CGRectMake([UIScreen mainScreen].bounds.size.width/2.0-100, 230, 200, 150)];
+    UIWebView *webView = [[UIWebView alloc] initWithFrame:CGRectMake(kX, 190, 200, 150)];
     webView.backgroundColor = [UIColor whiteColor];
     webView.scalesPageToFit = YES;
     [webView loadData:[self loadDataForIndex:1] MIMEType:@"image/gif" textEncodingName:@"" baseURL:[NSURL URLWithString:@""]];
     [self.view addSubview:webView];
+    
+    // SDWebImage
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(kX, 360, 200, 150)];
+    imageView.image = [UIImage sd_animatedGIFWithData:[self loadDataForIndex:1]];
+    [self.view addSubview:imageView];
+    
     // 帧动画
     [self runGifForImage];
 }
 
 - (void)runGifForImage {
-    UIImageView *gifImageView = [[UIImageView alloc] initWithFrame:CGRectMake([UIScreen mainScreen].bounds.size.width/2.0-100, 430, 200, 150)];
+    UIImageView *gifImageView = [[UIImageView alloc] initWithFrame:CGRectMake(kX, 530, 200, 150)];
     NSMutableArray *gifArray = [NSMutableArray array];
     for (NSInteger i = 1; i < 17; i++) {
         [gifArray addObject:[UIImage imageNamed:[NSString stringWithFormat:@"run－%ld", i]]];
